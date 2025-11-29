cb5ba80b06b1b97251c66378226348b093590cef
Matias Heikkilä <matias@bytesauna.com>
2025-11-29 16:13:13 +0200

    format

--- src/config.cpp
+++ src/config.cpp
@@ -1,27 +1,24 @@
 #include "config.h"
+#include <iostream>
+#include <ranges>
 #include <string>
 #include <string_view>
-#include <ranges>
-#include <iostream>
 
-inline const nlohmann::json& get_nested(const nlohmann::json& j,
-                                        const std::string& key)
-{
-    const nlohmann::json* ptr = &j;
+inline const nlohmann::json &get_nested(const nlohmann::json &j,
+                                        const std::string &key) {
+  const nlohmann::json *ptr = &j;
 
-    for (auto part_view : std::string_view(key) | std::views::split('.')) {
-        std::string part;
-        for (char c : part_view) {
-            part.push_back(c);
-        }
-
-        ptr = &ptr->at(part);
+  for (auto part_view : std::string_view(key) | std::views::split('.')) {
+    std::string part;
+    for (char c : part_view) {
+      part.push_back(c);
     }
 
-    return *ptr;
+    ptr = &ptr->at(part);
+  }
+
+  return *ptr;
 }
-
-
 
 template <typename T>
 void get_and_validate_param(T &config_field, const json &j,
@@ -29,7 +26,7 @@ void get_and_validate_param(T &config_field, const json &j,
                             std::function<bool(const T &)> validator,
                             const std::string &validation_error_message) {
   try {
-    const auto& val = get_nested(j, key);
+    const auto &val = get_nested(j, key);
     config_field = val.get<T>();
   } catch (const nlohmann::json::type_error &e) {
     throw std::runtime_error(
@@ -86,48 +83,55 @@ Params preprocessParams(const json &j) {
                                          "simulationMode", modeValidator,
                                          not_empty_message);
 
-  get_and_validate_param<float>(config.grossPitaevskii.L, j, "grossPitaevskii.L", is_positive,
+  get_and_validate_param<float>(config.grossPitaevskii.L, j,
+                                "grossPitaevskii.L", is_positive,
                                 positive_number_message);
 
-  get_and_validate_param<float>(config.grossPitaevskii.sigma, j, "grossPitaevskii.sigma",
-                                is_positive, positive_number_message);
-
-  get_and_validate_param<float>(config.grossPitaevskii.x0, j, "grossPitaevskii.x0", always_true,
-                                "");
-
-  get_and_validate_param<float>(config.grossPitaevskii.y0, j, "grossPitaevskii.y0", always_true,
-                                "");
-
-  get_and_validate_param<float>(config.grossPitaevskii.kx, j, "grossPitaevskii.kx", always_true,
-                                "");
-
-  get_and_validate_param<float>(config.grossPitaevskii.ky, j, "grossPitaevskii.ky", always_true,
-                                "");
-
-  get_and_validate_param<float>(config.grossPitaevskii.amp, j, "grossPitaevskii.amp",
-                                always_true, "");
-
-  get_and_validate_param<float>(config.grossPitaevskii.trapStr, j, "grossPitaevskii.trapStr",
-                                is_non_negtive, non_negtive_number_message);
-
-  get_and_validate_param<float>(config.grossPitaevskii.g, j, "grossPitaevskii.g", always_true,
-                                "");
-
-  get_and_validate_param<float>(config.grossPitaevskii.dt, j, "grossPitaevskii.dt", is_positive,
+  get_and_validate_param<float>(config.grossPitaevskii.sigma, j,
+                                "grossPitaevskii.sigma", is_positive,
                                 positive_number_message);
 
-  get_and_validate_param<float>(config.grossPitaevskii.V_bias, j, "grossPitaevskii.V_bias",
-                                is_non_negtive, non_negtive_number_message);
+  get_and_validate_param<float>(config.grossPitaevskii.x0, j,
+                                "grossPitaevskii.x0", always_true, "");
 
-  get_and_validate_param<float>(config.grossPitaevskii.r_0, j, "grossPitaevskii.r_0",
-                                is_non_negtive, non_negtive_number_message);
+  get_and_validate_param<float>(config.grossPitaevskii.y0, j,
+                                "grossPitaevskii.y0", always_true, "");
 
-  get_and_validate_param<float>(config.grossPitaevskii.sigma2, j, "grossPitaevskii.sigma2",
-                                is_non_negtive, non_negtive_number_message);
+  get_and_validate_param<float>(config.grossPitaevskii.kx, j,
+                                "grossPitaevskii.kx", always_true, "");
+
+  get_and_validate_param<float>(config.grossPitaevskii.ky, j,
+                                "grossPitaevskii.ky", always_true, "");
+
+  get_and_validate_param<float>(config.grossPitaevskii.amp, j,
+                                "grossPitaevskii.amp", always_true, "");
+
+  get_and_validate_param<float>(config.grossPitaevskii.trapStr, j,
+                                "grossPitaevskii.trapStr", is_non_negtive,
+                                non_negtive_number_message);
+
+  get_and_validate_param<float>(config.grossPitaevskii.g, j,
+                                "grossPitaevskii.g", always_true, "");
+
+  get_and_validate_param<float>(config.grossPitaevskii.dt, j,
+                                "grossPitaevskii.dt", is_positive,
+                                positive_number_message);
+
+  get_and_validate_param<float>(config.grossPitaevskii.V_bias, j,
+                                "grossPitaevskii.V_bias", is_non_negtive,
+                                non_negtive_number_message);
+
+  get_and_validate_param<float>(config.grossPitaevskii.r_0, j,
+                                "grossPitaevskii.r_0", is_non_negtive,
+                                non_negtive_number_message);
+
+  get_and_validate_param<float>(config.grossPitaevskii.sigma2, j,
+                                "grossPitaevskii.sigma2", is_non_negtive,
+                                non_negtive_number_message);
 
   get_and_validate_param<float>(config.grossPitaevskii.absorbStrength, j,
-                                "grossPitaevskii.absorbStrength", is_non_negtive,
-                                non_negtive_number_message);
+                                "grossPitaevskii.absorbStrength",
+                                is_non_negtive, non_negtive_number_message);
 
   get_and_validate_param<float>(config.grossPitaevskii.absorbWidth, j,
                                 "grossPitaevskii.absorbWidth", is_non_negtive,
